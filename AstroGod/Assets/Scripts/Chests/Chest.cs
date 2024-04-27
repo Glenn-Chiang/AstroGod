5c8f38ede53f12c8eb5f6e1b330266b91d122f54
Glenn Chiang <glennchiang24@gmail.com>
2024-04-27 11:49:13 +0800

    Add prefabs for opened versions of chests, which will be spawned to replace the original chest once it has been opened i.e. interacted with

--- AstroGod/Assets/Scripts/Chests/Chest.cs
+++ AstroGod/Assets/Scripts/Chests/Chest.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class Chest : Interactable
 {
     [SerializeField] private List<ItemPickUp> itemPrefabs;
+    [SerializeField] private GameObject openedChestPrefab; // This will replace the chest after it has been opened i.e. interacted with
 
     public override void OnInteract(InteractSystem interactSystem)
     {
         var itemPickUp = GetRandomItem();
         Instantiate(itemPickUp, transform.position, transform.rotation);
         Destroy(gameObject);
+        Instantiate(openedChestPrefab, transform.position, transform.rotation);
     }
 
     private ItemPickUp GetRandomItem()
