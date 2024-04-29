ff1122b1eccef1bb61c5b20ed2b3380a792f2d20
Glenn Chiang <glennchiang24@gmail.com>
2024-04-29 22:38:20 +0800

    Add level display script

--- AstroGod/Assets/Scripts/UI/XpDisplay.cs
+++ AstroGod/Assets/Scripts/UI/XpDisplay.cs
@@ -1,14 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class XpBar : MonoBehaviour
+public class XpDisplay : MonoBehaviour
 {
     [SerializeField] private XPManager xpManager;
     [SerializeField] private Slider slider;
+    [SerializeField] private TMP_Text levelDisplay;
 
     private void Update()
     {
         slider.maxValue = xpManager.xpPerLevel;
         slider.value = xpManager.CurrentLevelXp;
+        levelDisplay.text = $"LVL {xpManager.Level}";
     }
 }
