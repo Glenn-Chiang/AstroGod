cca31fca4b2df88ac05c2563af586711ea177c9f
Glenn Chiang <glennchiang24@gmail.com>
2024-05-04 17:16:14 +0800

    Add room generator

---
+++ AstroGod/Assets/Scripts/LevelGeneration/RoomGenerator.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+    private int roomCount = 8;
+    [SerializeField] private int rowCount = 4;
+    [SerializeField] private int colCount = 4;
+    [SerializeField] private float gap = 2;
+    private Vector3 origin;
+    [SerializeField] private GameObject roomPrefab;
+    private Vector3 RoomSize => roomPrefab.GetComponent<Renderer>().bounds.size;
+    private GameObject[,] rooms;
+
+    private void Awake()
+    {
+        origin = transform.position;
+        rooms = new GameObject[rowCount, colCount];
+        GenerateRooms();
+    }
+    
+    public void GenerateRooms()
+    {
+        var randomWalker = new RandomWalker(rowCount, colCount, roomCount);
+        randomWalker.Walk();
+        var grid = randomWalker.grid;
+
+        for (int row = 0; row < rowCount; row++)
+        {
+            for (int col = 0; col < colCount; col++)
+            {
+                if (!grid[row, col]) continue;
+
+                var roomX = origin.x + col * (RoomSize.x + gap);
+                var roomY = origin.y - row * (RoomSize.y + gap);
+                var roomPosition = new Vector2(roomX, roomY);
+                var room = Instantiate(roomPrefab, roomPosition, Quaternion.identity);
+                rooms[row, col] = room;                
+            }
+        }
+    }
+}
